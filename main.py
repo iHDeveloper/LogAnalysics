c83cee9ac6073840a12f8bdf18ecc32f85b8e655
iHDeveloper <hamzadevelop@gmail.com>
2018-11-06 18:33:00 +0300

    Apply the pep8 style

--- main.py
+++ main.py
@@ -2,14 +2,24 @@
 
 import psycopg2
 
-sql_most_articles = "SELECT articles.slug, count(*) as Counts FROM log JOIN articles ON CONCAT('/article/', articles.slug) = log.path GROUP BY articles.slug ORDER BY Counts desc LIMIT 5;"
-sql_most_author = "SELECT authors.name, count(*) as Counts FROM log JOIN articles ON CONCAT('/article/', articles.slug) = log.path JOIN authors ON authors.id = articles.author GROUP BY authors.name ORDER BY Counts desc;"
-sql_errors = "SELECT time::timestamp::date as date, (((SELECT count(*) FROM log WHERE log.time = time::timestamp::date) * 100.0 ) / count(*)) AS Counter FROM log WHERE NOT status = '200 OK' GROUP BY date ORDER BY Counter LIMIT 1;"
+sql_most_articles = "SELECT articles.slug, count(*) as Counts FROM log \
+JOIN articles ON CONCAT('/article/', articles.slug) = \
+log.path GROUP BY articles.slug ORDER BY Counts desc LIMIT 5;"
+sql_most_author = "SELECT authors.name, count(*) as Counts FROM log \
+JOIN articles ON CONCAT('/article/', articles.slug) = \
+log.path JOIN authors ON authors.id = articles.author \
+GROUP BY authors.name ORDER BY Counts desc;"
+sql_errors = "SELECT time::timestamp::date as date, \
+(((SELECT count(*) FROM log WHERE log.time = time::timestamp::date) * 100.0 ) \
+/ count(*)) \
+AS Counter FROM log WHERE \
+ NOT status = '200 OK' GROUP BY date ORDER BY Counter LIMIT 1;"
 
 try:
         conn = psycopg2.connect("dbname=news")
 except:
         print ('Unable to connect to the database')
+
 
 # Execute SQL query
 def execute(query):
@@ -19,6 +29,7 @@ def execute(query):
     cur.close()
     return r
 
+
 # Print a array with count like 1, 2, 3
 def most(query, key):
     result = execute(query)
@@ -27,6 +38,7 @@ def most(query, key):
         title = obj[0]
         gets = obj[1]
         print('#', (i + 1), ' | ' + title, ' -> ', gets, key)
+
 
 # Execute the custom Query and print it in custom format
 def moreErrors():
