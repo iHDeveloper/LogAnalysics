0a7eaf129d39fb7efc85d27ded14139b89c80c39
iHDeveloper <hamzadevelop@gmail.com>
2018-11-06 17:20:09 +0300

    Fix some problems

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import psycopg2
 
 sql_most_articles = "SELECT articles.slug, count(*) as Counts FROM log JOIN articles ON CONCAT('/article/', articles.slug) = log.path GROUP BY articles.slug ORDER BY Counts desc LIMIT 5;"
 sql_most_author = "SELECT authors.name, count(*) as Counts FROM log JOIN articles ON CONCAT('/article/', articles.slug) = log.path JOIN authors ON authors.id = articles.author GROUP BY authors.name ORDER BY Counts desc;"
-sql_errors = "SELECT time::timestamp::date as date, (((SELECT count(*) FROM log WHERE log.time = time::timestamp::date) * 100.0 ) / count(*)) AS Counter FROM log WHERE NOT status = '200 OK' GROUP BY date;"
+sql_errors = "SELECT time::timestamp::date as date, (((SELECT count(*) FROM log WHERE log.time = time::timestamp::date) * 100.0 ) / count(*)) AS Counter FROM log WHERE NOT status = '200 OK' GROUP BY date ORDER BY Counter LIMIT 1;"
 
-conn = psycopg2.connect("dbname=news")
+try:
+        conn = psycopg2.connect("dbname=news host=35.187.32.92 user=postgres password=123")
+except:
+        print ('Unable to connect to the database')
 
 # Execute SQL query
 def execute(query):
@@ -28,11 +31,10 @@ def most(query, key):
 # Execute the custom Query and print it in custom format
 def moreErrors():
     result = execute(sql_errors)
-    for i in range(len(result)):
-        obj = result[i]
-        date = obj[0]
-        time = obj[1]
-        print(date.strftime('%b %d, %Y'), ' -- ', str(time)[0: 3], "%% errors")
+    r = result[0]
+    date = r[0]
+    time = r[1]
+    print ('>', date, 'which it was', '%s%%' % str(time)[0: 3])
 
 print("Q1: What are the most popular three articles of all time?")
 most(sql_most_articles, 'views')
@@ -44,6 +46,6 @@ most(sql_most_author, 'views')
 print()
 print("----------------------------")
 print()
-print("Q3: On which days did more than 1%% of requests lead to errors?")
+print('Q3: On which days did more than %s%% of requests lead to errors?' % '1')
 moreErrors()
-conn.close() # Close the SQL Connection
+conn.close()  # Close the SQL Connection
